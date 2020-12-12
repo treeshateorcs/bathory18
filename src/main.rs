72dfa735f9345b961f1e1dde138c3aaf65dbd3e3
treeshateorcs <trees@hateorcs.com>
2020-12-11 20:06:02 -0800

    compilation fail

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-use chrono::TimeZone;
 use std::io::BufRead;
 use std::io::Read;
 use std::io::Write;
 
 const TIMEOUT: u64 = 60;
-const LAST_ARTICLES: usize = 100; // this many articles to display
 
 #[derive(PartialEq)]
 struct Item {
   read: i8,
-  timestamp: chrono::DateTime<chrono::Utc>,
+  timestamp: i64,
   feed_title: String,
   command: String,
-  item: feed_rs::model::Entry,
+  article_title: String,
+  link: String,
 }
 
 #[derive(serde::Deserialize)]
@@ -22,21 +21,25 @@ struct ReadItem {
 }
 
 fn main() {
-  let xdg_dirs = xdg::BaseDirectories::new().unwrap();
-  let lock = xdg_dirs.place_config_file("bathory18/lock").unwrap();
+  let config_dir = dirs::config_dir().unwrap();
+  let mut lock = config_dir.clone();
+  lock.push("bathory18/lock");
   let instance_a = single_instance::SingleInstance::new(lock.to_str().unwrap()).unwrap();
   if !instance_a.is_single() {
-    println!("only one instance of swkb at a time is allowed");
+    println!(
+      "only one instance of {:?} at a time is allowed",
+      std::env::args().take(1)
+    );
     std::process::exit(1);
   }
-  let urls = xdg_dirs.place_config_file("bathory18/urls").unwrap();
-  let urls = std::fs::File::open(&urls).expect(
-    format!(
+  let mut urls = config_dir;
+  urls.push("bathory18/urls");
+  let urls = std::fs::File::open(&urls).unwrap_or_else(|_| {
+    panic!(
       "create file {} and add your feeds in it",
       urls.to_str().unwrap()
     )
-    .as_str(),
-  );
+  });
   let mut reader = std::io::BufReader::new(urls);
   let mut lines = Vec::new();
   for line in reader.by_ref().lines() {
@@ -52,7 +55,7 @@ fn main() {
     let mut feeds: Vec<Item> = Vec::new();
     let feed_threads = parse_feeds(&lines);
     for feed_thread in feed_threads {
-      let fee = feed_thread.join().unwrap_or(Vec::new());
+      let fee = feed_thread.join().unwrap_or_default();
       for f in fee {
         feeds.push(f);
       }
@@ -60,18 +63,9 @@ fn main() {
     remove_read_items(&mut feeds);
     feeds.sort_unstable_by(|a, b| {
       if b.timestamp != a.timestamp {
-        a.timestamp
-          .timestamp()
-          .partial_cmp(&b.timestamp.timestamp())
-          .unwrap()
-      } else if b.item.title != a.item.title {
-        b.item
-          .title
-          .as_ref()
-          .unwrap()
-          .content
-          .partial_cmp(&a.item.title.as_ref().unwrap().content)
-          .unwrap()
+        a.timestamp.partial_cmp(&b.timestamp).unwrap()
+      } else if b.article_title != a.article_title {
+        b.article_title.partial_cmp(&a.article_title).unwrap()
       } else {
         b.feed_title.partial_cmp(&a.feed_title).unwrap()
       }
@@ -80,28 +74,45 @@ fn main() {
       mark_all_as_read(&mut feeds);
       first_time = false;
     }
-    for article in feeds.iter().rev().take(LAST_ARTICLES) {
-      let args = vec![article.item.links[0].href.clone()];
-      notify_rust::Notification::new()
-        .summary(article.feed_title.as_str())
-        .body(article.item.title.as_ref().unwrap().content.as_str())
-        .timeout(notify_rust::Timeout::Never)
-        .action("default", "default")
-        .show()
-        .unwrap()
-        .wait_for_action(|action| match action {
-          "default" => open_url(&article.command, &args),
-          _ => (),
-        });
+    for article in feeds.iter().rev() {
+      notify(&article);
       mark_as_read(article);
     }
     std::thread::sleep(std::time::Duration::from_secs(TIMEOUT));
   }
 }
 
+#[cfg(target_os = "windows")]
+fn notify(article: &Item) {
+  notify_rust::Notification::new()
+    .summary(article.feed_title.as_str())
+    .body(&article.article_title)
+    .timeout(notify_rust::Timeout::Never)
+    .action("default", "default")
+    .show()
+    .unwrap();
+}
+
+#[cfg(not(target_os = "windows"))]
+fn notify(article: &Item) {
+  let args = vec![&article.link];
+  notify_rust::Notification::new()
+    .summary(article.feed_title.as_str())
+    .body(&article.article_title)
+    .timeout(notify_rust::Timeout::Never)
+    .action("default", "default")
+    .show()
+    .unwrap()
+    .wait_for_action(|action| {
+      if let "default" = action {
+        open_url(&article.command, &args);
+      }
+    });
+}
+
 fn mark_all_as_read(entries: &mut Vec<Item>) {
-  let xdg_dirs = xdg::BaseDirectories::new().unwrap();
-  let read = xdg_dirs.place_config_file("bathory18/read").unwrap();
+  let mut read = dirs::config_dir().unwrap();
+  read.push("bathory18/read");
   let mut file = std::fs::OpenOptions::new()
     .read(true)
     .append(true)
@@ -112,15 +123,7 @@ fn mark_all_as_read(entries: &mut Vec<Item>) {
   for entry in entries {
     if entry.read == 0 {
       entry.read = 1;
-      write.push_str(&format!(
-        "{},{}\n",
-        entry
-          .item
-          .published
-          .unwrap_or_else(|| chrono::Utc.timestamp(0, 0))
-          .timestamp(),
-        entry.item.links[0].href
-      ))
+      write.push_str(&format!("{},{}\n", entry.timestamp, entry.link))
     }
   }
   file.write(write.as_bytes()).ok();
@@ -130,7 +133,7 @@ fn parse_feeds(lines: &Vec<String>) -> Vec<std::thread::JoinHandle<Vec<Item>>> {
   let mut feed_threads = Vec::new();
   for line in lines.clone() {
     feed_threads.push(std::thread::spawn(move || -> Vec<Item> {
-      let mut url_title_command_vec: Vec<_> = line.splitn(3, ",").collect();
+      let mut url_title_command_vec: Vec<_> = line.splitn(3, ',').collect();
       while url_title_command_vec.len() < 3 {
         url_title_command_vec.push("");
       }
@@ -155,7 +158,7 @@ fn parse_feeds(lines: &Vec<String>) -> Vec<std::thread::JoinHandle<Vec<Item>>> {
           return feeds;
         }
       };
-      if title.len() == 0 {
+      if title.is_empty() {
         title = match feed.title {
           Some(t) => t.content,
           None => String::new(),
@@ -176,29 +179,27 @@ fn parse_feeds(lines: &Vec<String>) -> Vec<std::thread::JoinHandle<Vec<Item>>> {
           read: 0,
           feed_title: title.clone(),
           command: command.clone(),
+          article_title: entry.title.unwrap().content,
           timestamp: match entry.published {
-            Some(t) => t,
+            Some(t) => t.timestamp(),
             None => match entry.updated {
-              Some(t) => t,
-              None => chrono::DateTime::<chrono::Utc>::from_utc(
-                chrono::NaiveDateTime::from_timestamp(1, 0),
-                chrono::Utc,
-              ),
+              Some(t) => t.timestamp(),
+              None => 1,
             },
           },
-          item: entry,
+          link: entry.links[0].href.clone(),
         };
         feeds.push(i);
       }
       feeds
     }));
   }
-  return feed_threads;
+  feed_threads
 }
 
 fn remove_read_items(entries: &mut Vec<Item>) {
-  let xdg_dirs = xdg::BaseDirectories::new().unwrap();
-  let read = xdg_dirs.place_config_file("bathory18/read").unwrap();
+  let mut read = dirs::config_dir().unwrap();
+  read.push("bathory18/read");
   let file = std::fs::read_to_string(read).unwrap_or_else(|_| "0,\"\"".to_string());
   let mut rdr = csv::ReaderBuilder::new()
     .has_headers(false)
@@ -210,31 +211,22 @@ fn remove_read_items(entries: &mut Vec<Item>) {
     read_items.push(row);
   }
   for row in read_items {
-    entries.retain(|entry| {
-      !(entry
-        .item
-        .published
-        .unwrap_or_else(|| chrono::Utc.timestamp(0, 0))
-        .timestamp()
-        == row.date
-        && entry.item.links[0].href == row.link)
-    });
+    entries.retain(|entry| !(entry.timestamp == row.date && entry.link == row.link));
   }
 }
-
-fn open_url(command: &String, args: &Vec<String>) {
+#[cfg(not(target_os = "windows"))]
+fn open_url(command: &str, args: &Vec<&String>) {
   std::process::Command::new(command)
     .args(args)
     .stdout(std::process::Stdio::null())
     .stderr(std::process::Stdio::null())
     .spawn()
-    .ok()
     .expect("failed to spawn process");
 }
 
 fn mark_as_read(entry: &Item) {
-  let xdg_dirs = xdg::BaseDirectories::new().unwrap();
-  let read = xdg_dirs.place_config_file("bathory18/read").unwrap();
+  let mut read = dirs::config_dir().unwrap();
+  read.push("bathory18/read");
   let mut file = std::fs::OpenOptions::new()
     .read(true)
     .append(true)
@@ -247,29 +239,11 @@ fn mark_as_read(entry: &Item) {
   for result in rdr.records() {
     let record = result.unwrap();
     let row: ReadItem = record.deserialize(None).unwrap();
-    if row.date
-      == entry
-        .item
-        .published
-        .unwrap_or_else(|| chrono::Utc.timestamp(0, 0))
-        .timestamp()
-      && entry.item.links[0].href == row.link
-    {
+    if row.date == entry.timestamp && entry.link == row.link {
       return;
     }
   }
   file
-    .write(
-      format!(
-        "{},{}\n",
-        entry
-          .item
-          .published
-          .unwrap_or_else(|| chrono::Utc.timestamp(0, 0))
-          .timestamp(),
-        entry.item.links[0].href
-      )
-      .as_bytes(),
-    )
+    .write(format!("{},{}\n", entry.timestamp, entry.link).as_bytes())
     .ok();
 }
